a82577037bb30b4af584a98f84a66d25ab50bd1b
gupta22-dev <immortal@2020>
2021-04-05 19:28:37 +0530

    master

---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(HomePage());
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  AnimationController rippleController;
+  AnimationController scaleController;
+
+  Animation<double> rippleAnimation;
+  Animation<double> scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    rippleController =
+        AnimationController(vsync: this, duration: Duration(seconds: 1));
+    scaleController =
+        AnimationController(vsync: this, duration: Duration(seconds: 10));
+
+    rippleAnimation =
+        Tween<double>(begin: 80.0, end: 90.0).animate(rippleController)
+          ..addStatusListener((status) {
+            if (status == AnimationStatus.completed) {
+              rippleController.reverse();
+            } else if (status == AnimationStatus.dismissed) {
+              rippleController.forward();
+            }
+          });
+    scaleAnimation =
+        Tween<double>(begin: 1.0, end: 30.0).animate(scaleController);
+
+    rippleController.forward();
+  }
+
+  @override
+  void dispose() {
+    rippleController.dispose();
+    scaleController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Align(
+        alignment: Alignment.bottomCenter,
+        child: AnimatedBuilder(
+          animation: rippleAnimation,
+          builder: (context, child) => Container(
+            width: rippleAnimation.value,
+            height: rippleAnimation.value,
+            child: Container(
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle, color: Colors.blue.withOpacity(0.4)),
+              child: InkWell(
+                onTap: () {
+                  scaleController.forward();
+                },
+                child: AnimatedBuilder(
+                    animation: scaleAnimation,
+                    builder: (context, child) => Transform.scale(
+                          scale: scaleAnimation.value,
+                          child: Container(
+                              margin: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle, color: Colors.blue)),
+                        )),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
